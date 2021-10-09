43e493a2971a29ac2e0acef95b4141678d06becc
ayush6635 <ayush.mishra2019@vitstudent.ac.in>
2021-10-09 19:46:28 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+
+	"github.com/ayush6635/appointyTaskApi/controllers"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+var err error
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
